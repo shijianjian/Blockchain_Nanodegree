d365f2e166970901de540c728b4d733e0e8734e0
Jian S <sj8716643@126.com>
2020-02-15 18:39:31 +0800

    Fixed naming issue.

--- capstone_decentralized_housing_product/eth-contracts/migrations/2_deploy_contracts.js
+++ capstone_decentralized_housing_product/eth-contracts/migrations/2_deploy_contracts.js
@@ -1,10 +1,10 @@
 // migrating the appropriate contracts
 var ERC721TokenHousing = artifacts.require("./ERC721TokenHousing.sol")
-var Verifier = artifacts.require("./SquareVerifier.sol");
+var SquareVerifier = artifacts.require("./SquareVerifier.sol");
 // var SolnSquareVerifier = artifacts.require("./SolnSquareVerifier.sol");
 
 module.exports = function(deployer) {
   deployer.deploy(ERC721TokenHousing);
-  deployer.deploy(Verifier);
+  deployer.deploy(SquareVerifier);
   // deployer.deploy(SolnSquareVerifier);
 };
